2142f99cc60c8fa073e874c059d936ca7fa8ac11
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-09-08 20:14:46 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-name_has.delete_if {|key, value| key >=0}
+name_hash.delete_if {|key, value| key >=0}
 end
